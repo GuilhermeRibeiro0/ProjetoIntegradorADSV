06e78d5840cb9f81bb1064d4a8ad9190f22cb99f
GuilhermeRibeiro0 <guilhermeribeir0@hotmail.com>
2022-05-31 15:27:44 -0300

    cliente model e repository criados

---
+++ src/main/java/br/com/pi/model/Cliente.java
@@ -0,0 +1,63 @@
+package br.com.pi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cliente_id")
+	private Long id;
+	
+	@Column(name = "cliente_nome")
+	private String nome;
+	
+	@Column(name = "cliente_cpf")
+	private String cpf;
+	
+	@Column(name = "cliente_cpf")
+	private String telefone;
+	
+	@Column(name = "cliente_cpf")
+	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
