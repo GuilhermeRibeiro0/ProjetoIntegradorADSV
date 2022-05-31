628a5beaeea2662c4ed3d0af5caba2826110fd21
GuilhermeRibeiro0 <guilhermeribeir0@hotmail.com>
2022-05-31 16:13:11 -0300

    model endereco

---
+++ src/main/java/br/com/pi/model/Endereco.java
@@ -0,0 +1,94 @@
+package br.com.pi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "endereco_id")
+	private Long id;
+	
+	@Column(name = "end_logradouro")
+	private String logradouro;
+	
+	@Column(name = "end_numero")
+	private String numero;
+	
+	@Column(name = "end_complemento")
+	private String complemento;
+	
+	@Column(name = "end_bairro")
+	private String bairro;
+	
+	@Column(name = "end_cidade")
+	private String cidade;
+	
+	@Column(name = "end_estado")
+	private String estado;
+	
+	@Column(name = "end_cep")
+	private String cep;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	
+
+
+
+}
