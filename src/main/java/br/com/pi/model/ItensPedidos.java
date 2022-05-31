573a057e3d2745a36e97ea149f6c9298b4f1a7ff
GuilhermeRibeiro0 <guilhermeribeir0@hotmail.com>
2022-05-31 17:14:01 -0300

    pedidos finalizado

---
+++ src/main/java/br/com/pi/model/ItensPedidos.java
@@ -0,0 +1,44 @@
+package br.com.pi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class ItensPedidos {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "pedidos_id")
+	private Long id;
+	
+	@Column(name = "pedidos_quantidade")
+	private String quantidade;
+	
+	@Column(name = "pedidos_valor")
+	private Float valorUnitario;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Float getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(Float valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	
+	
+}
